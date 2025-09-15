a2dc88eb7f8b1792da7feb9b9116891065c70c91
DhruvMadan05 <Dhruv.madan05@gmail.com>
2025-09-14 21:35:42 -0400

    Added test file to run variants as many times as wanted

--- teamNN/project1/variant1.py
+++ teamNN/project1/variant1.py
@@ -1,10 +1,10 @@
 # This is necessary to find the main code
 import sys
-sys.path.insert(0, '../../bomberman')
+sys.path.insert(0, '../../Bomberman')
 sys.path.insert(1, '..')
 
 # Import necessary stuff
-from game import Game
+from Bomberman.game import Game
 
 # TODO This is your code!
 sys.path.insert(1, '../teamNN')
