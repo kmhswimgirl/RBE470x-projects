cc0b37b3679f839861e832c3408a6e5d0b56bc81
DhruvMadan05 <Dhruv.madan05@gmail.com>
2025-10-05 17:25:50 -0400

    Misc changes from final push

--- team10/project1/variant2.py
+++ team10/project1/variant2.py
@@ -13,7 +13,7 @@ sys.path.insert(1, '../teamNN')
 from testcharacter2 import TestCharacter2
 
 # Create the game
-random.seed(123) # TODO Change this if you want different random choices
+random.seed() # TODO Change this if you want different random choices
 g = Game.fromfile('map.txt')
 g.add_monster(StupidMonster("stupid", # name
                             "S",      # avatar
